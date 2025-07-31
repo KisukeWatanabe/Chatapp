4f29b65dc85e16ec438c321ab2d62fa6fa5e17f5
Keito_T <2312110211y@kindai.ac.jp>
2025-07-31 14:04:21 +0900

    changeTaskからマージ

--- chatapp/src/components/Login.vue
+++ chatapp/src/components/Login.vue
@@ -36,8 +36,9 @@ const onEnter = () => {
 </script>
 
 <template>
-  <div class="mx-auto my-5 px-4">
-    <h1 class="text-h3 font-weight-medium">Vue.js Chat サンプル</h1>
+  <div class="mx-auto my-5 px-4 text-center">
+    <h1 class="text-h3 font-weight-medium">たすく はあくん</h1>
+    <img src="../images/image.png" width="500" height="300" style="position: fixed; top: 0; right: 0; z-index: -999;">
     <div class="mt-10">
       <p>ユーザー名</p>
       <input type="text" class="user-name-text" v-model="inputUserName"/>
